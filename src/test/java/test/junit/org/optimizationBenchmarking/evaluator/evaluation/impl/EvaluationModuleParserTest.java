bb83ace8251f450b7414781a86dedc37890e17c2
Thomas Weise <tweise@ustc.edu.cn>
2016-04-14 14:03:46 +0800

    Fixed Error in EvaluationModuleParserTest

--- src/test/java/test/junit/org/optimizationBenchmarking/evaluator/evaluation/impl/EvaluationModuleParserTest.java
+++ src/test/java/test/junit/org/optimizationBenchmarking/evaluator/evaluation/impl/EvaluationModuleParserTest.java
@@ -1,12 +1,14 @@
 package test.junit.org.optimizationBenchmarking.evaluator.evaluation.impl;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.optimizationBenchmarking.evaluator.attributes.functions.ecdf.ECDF;
 import org.optimizationBenchmarking.evaluator.evaluation.impl.EvaluationModuleParser;
 import org.optimizationBenchmarking.evaluator.evaluation.impl.all.aggregation2D.AllAggregation2D;
+import org.optimizationBenchmarking.evaluator.evaluation.impl.all.ecdf.AllECDF;
 import org.optimizationBenchmarking.evaluator.evaluation.impl.description.instances.InstanceInformation;
 import org.optimizationBenchmarking.evaluator.evaluation.impl.single.experimentInfo.ExperimentInformation;
+import org.optimizationBenchmarking.evaluator.evaluation.spec.IEvaluationModule;
 
 import shared.junit.InstanceTest;
 
@@ -19,58 +21,63 @@ public class EvaluationModuleParserTest
     super(null, EvaluationModuleParser.getInstance(), true, false);
   }
 
-  /** test for a particular module */
-  @Test(timeout = 3600000)
-  public void testAllAggregation2D() {
+  /**
+   * test for a particular module
+   *
+   * @param clazz
+   *          the expected base class
+   * @param names
+   *          the module names
+   */
+  @Ignore
+  private final void __testParse(
+      final Class<? extends IEvaluationModule> clazz,
+      final String... names) {
     final EvaluationModuleParser parser;
+    final IEvaluationModule module;
+
+    Assert.assertNotNull(clazz);
+    Assert.assertNotNull(names);
+    Assert.assertTrue(names.length > 0);
 
     parser = this.getInstance();
     Assert.assertNotNull(parser);
 
-    Assert.assertNotNull(
-        parser.parseString("aggregation2D.AllAggregation2D")); //$NON-NLS-1$
-    Assert.assertNotNull(
-        parser.parseString(AllAggregation2D.class.getCanonicalName()));
+    module = parser.parseString(clazz.getCanonicalName());
+    Assert.assertNotNull(module);
+    Assert.assertTrue(clazz.isInstance(module));
+    Assert.assertNotNull(module.getType());
+
+    for (final String name : names) {
+      Assert.assertEquals(module, parser.parseString(name));
+    }
+  }
+
+  /** test for a particular module */
+  @Test(timeout = 3600000)
+  public void testAllAggregation2D() {
+    this.__testParse(AllAggregation2D.class, //
+        "aggregation2D.AllAggregation2D"); //$NON-NLS-1$
   }
 
   /** test for a particular module */
   @Test(timeout = 3600000)
   public void testAllECDF() {
-    final EvaluationModuleParser parser;
-
-    parser = this.getInstance();
-    Assert.assertNotNull(parser);
-
-    Assert.assertNotNull(parser.parseString("ecdf.AllECDF")); //$NON-NLS-1$
-    Assert
-        .assertNotNull(parser.parseString(ECDF.class.getCanonicalName()));
+    this.__testParse(AllECDF.class, //
+        "ecdf.AllECDF"); //$NON-NLS-1$
   }
 
   /** test for a particular module */
   @Test(timeout = 3600000)
   public void testInstanceInformation() {
-    final EvaluationModuleParser parser;
-
-    parser = this.getInstance();
-    Assert.assertNotNull(parser);
-
-    Assert.assertNotNull(
-        parser.parseString("instances.InstanceInformation")); //$NON-NLS-1$
-    Assert.assertNotNull(
-        parser.parseString(InstanceInformation.class.getCanonicalName()));
+    this.__testParse(InstanceInformation.class, //
+        "instances.InstanceInformation"); //$NON-NLS-1$
   }
 
   /** test for a particular module */
   @Test(timeout = 3600000)
   public void testExperimentExperimentInformation() {
-    final EvaluationModuleParser parser;
-
-    parser = this.getInstance();
-    Assert.assertNotNull(parser);
-
-    Assert.assertNotNull(
-        parser.parseString("experimentInfo.ExperimentInformation")); //$NON-NLS-1$
-    Assert.assertNotNull(parser
-        .parseString(ExperimentInformation.class.getCanonicalName()));
+    this.__testParse(ExperimentInformation.class, //
+        "experimentInfo.ExperimentInformation"); //$NON-NLS-1$
   }
 }
